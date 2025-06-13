0a4dfdb525d03ff30af38f66f1a5b0a38d0d5173
David <owolagbadavid@gmail.com>
2025-06-14 00:17:03 +0100

    fix

--- src/auth/dtos/reset-password.dto.ts
+++ src/auth/dtos/reset-password.dto.ts
@@ -1,19 +1,22 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 import { HelperService } from 'src/services/helper.service';
 
 export class ResetPasswordDto {
   @ApiPropertyOptional()
+  @IsOptional()
   @IsString()
   @HelperService.Normalize()
   emailAddress?: string;
 
   @ApiPropertyOptional()
+  @IsOptional()
   @IsString()
   @HelperService.Normalize()
   username?: string;
 
   @ApiPropertyOptional()
+  @IsOptional()
   @IsString()
   @HelperService.Normalize()
   phoneNumber?: string;
